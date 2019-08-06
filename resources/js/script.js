df57680239b4f808d19a37e4b45947d548abe506
Zhana <zhana.jikia.1@btu.edu.ge>
2019-08-06 17:40:20 +0400

    Finish Project

---
+++ resources/js/script.js
@@ -0,0 +1,117 @@
+$(document).ready(function(){
+    // Sticky nav
+  $('.js--section-features').waypoint(function(direction){
+      if (direction == "down") {
+          $('nav').addClass('sticky');
+      }else {
+          $('nav').removeClass('sticky');
+      }
+  }, {
+      offset: '60px;'
+  });
+
+  // Scroll on buttons
+
+  $('.js--scroll-to-plans').click(function(){
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000)
+  });
+
+  $('.js--scroll-to-start').click(function(){
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000)
+  });
+
+  // Navigation scroll
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+//   animations on scroll
+  $('.js--wp-1').waypoint(function(direction){
+      $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+        offset: '50%'
+    });
+
+   $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+   }, {
+        offset: '50%'
+    });
+
+   $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+   }, {
+        offset: '50%'
+    });
+
+    // Mobile nav 'burger'
+  $('.js--nav-icon').click(function(){
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if(icon.hasClass('icon ion-md-menu')) {
+      icon.addClass('icon ion-md-close');
+      icon.removeClass('icon ion-md-menu');
+    } else {
+      icon.addClass('icon ion-md-menu');
+      icon.removeClass('icon ion-md-close');
+    }
+  });
+
+    // Maps
+    var map = new GMaps({
+      div: '.map',
+      lat: 38.7223654,
+      lng: -9.2093772,
+      lng: -9.05,
+      zoom: 12,
+    });
+
+    map.addMarker({
+      lat: 38.7223654,
+      lng: -9.2093772,
+      title: 'lisbon',
+      infoWindow: {
+        content: '<p>Lisbon Here</p>'
+      }
+    });
+});
+
